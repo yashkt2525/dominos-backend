bdd256ca7e53e2b76455151083758b124aee5ed5
Yash Tyagi <92726355+yashkt2525@users.noreply.github.com>
2024-07-24 12:15:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
+app.use(cors());
 app.use(express.json());
 const PORT = process.env.PORT || 1234;
 app.listen(PORT, () => console.log(`App starting at PORT ${PORT}`));
