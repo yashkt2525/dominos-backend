4f0ca2c0056666f46c2c2d97a58df0fda8053121
Yash Tyagi <92726355+yashkt2525@users.noreply.github.com>
2024-07-24 11:59:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+app.use(express.json());
+const PORT = process.env.PORT || 1234;
+app.listen(PORT, () => console.log(`App starting at PORT ${PORT}`));
+
+mongoose
+  .connect(
+    "mongodb+srv://yashdb:BQ813OVBsitrfo4f@cluster0.3ypsu0u.mongodb.net/dominos"
+  )
+  .then(() => console.log("SuccessFully Cnnected With Mongodb"))
+  .catch((err) => console.log(err));
+
+const productSchema = new mongoose.Schema({
+  product_id: String,
+  product_name: String,
+  toppings: [String],
+  price: Number,
+  size: String,
+});
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  ph_no: String,
+  gender: String,
+  user_id: Number,
+  id_deleted: Boolean,
+});
+
+const Product = mongoose.model("products", productSchema);
+const User = mongoose.model("users", userSchema);
+
+app.get("/", (req, res) => {
+  res.json({ name: "Hello", age: 10, weight: 10 });
+});
+app.get("/get-products", async (req, res) => {
+  const products = await Product.find();
+  res.json(products);
+});
+
+app.post("/user", async (req, res) => {
+  const user = req.body;
+  console.log(user);
+  const exist = await User.findOne({ user_id: user.user_id });
+  console.log(exist);
+  if (!exist) {
+    User.create(user);
+    res.send("user created Succesfully");
+  }
+  res.json("Already Exists");
+});
+
+app.get("/get-users", async (req, res) => {
+  const users = await User.find();
+  res.json(users);
+});
